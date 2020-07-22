687622ef3421d30b1e83e9058637802dad5c376d
YaninaMagali <yani.m.diaz@gmail.com>
2020-07-22 13:49:01 -0300

    Corrijo funcion Materiales()

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -1,4 +1,4 @@
-/*2.	Para el departamento de Construcción:
+﻿/*2.	Para el departamento de Construcción:
 A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
@@ -27,6 +27,6 @@ function Materiales ()
     var ancho = parseFloat(document.getElementById("txtIdAncho").value);
     var area = largo*ancho;
 
-    alert("debe comprar " + (area * 2 / 2) + " bolsas de cemento y " + (area * 3 / 2) + " bolsas de cal");
+    alert("debe comprar " + (area * 2) + " bolsas de cemento y " + (area * 3) + " bolsas de cal");
 	
 }
